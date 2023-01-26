a3e9ec8a9fc6f1477bb1a982f014954b1b8bb2c1
SoaresRo <rosangela.soares.qa@gmail.com>
2023-01-26 20:48:05 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 
 public class Main {
@@ -23,9 +23,41 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        BootCamp bootCamp = new BootCamp();
+        bootCamp.setNome("Bootcamp Java Developer");
+        bootCamp.setDescricao("Descrição Bottcamp Java Developer");
+        bootCamp.getNome().add(curso1);
+        bootCamp.getNome().add(curso2);
+        bootCamp.getNome().add(mentoria);
+
+        Dev devRosangela = new Dev();
+        devRosangela.setNome("Rosangela");
+        devRosangela.inscreverBootcamp(bootCamp);
+        System.out.println("Conteudos Inscritos Rosangela" + devRosangela.getConteudosInscritos());
+        devRosangela.progredir();
+        devRosangela.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos Rosangela" + devRosangela.getConteudosConcluidos());
+        System.out.println("XP: " + devRosangela.calcularTotalXp());
+
+
+        System.out.println("-------");
+
+        Dev devRebecca = new Dev();
+        devRebecca.setNome("Rebecca");
+        devRebecca.inscreverBootcamp(bootCamp);
+        System.out.println("Conteudos Inscritos Rebecca" + devRebecca.getConteudosInscritos());
+        devRebecca.progredir();
+        devRebecca.progredir();
+        devRebecca.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos Concluidos Rebecca" + devRebecca.getConteudosConcluidos());
+        System.out.println("XP: " + devRebecca.calcularTotalXp());
+        System.out.println("-------");
 
     }
 
